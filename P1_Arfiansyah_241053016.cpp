055e72d6585614009e13087ccbdddedcb3a9a74d
AverA45 <averadetya4512@gmail.com>
2025-07-01 12:09:29 +0700

    terakhir

--- P1_Arfiansyah_241053016.cpp
+++ P1_Arfiansyah_241053016.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
-//sdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+
 using namespace std;
 //ASDASDASDASDSADASDASDADASDASDSADAS
 struct KTPNode {
